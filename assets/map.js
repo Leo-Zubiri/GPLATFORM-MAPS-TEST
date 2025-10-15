d528a994ac587fd5066fd649f22fbae19d4ac97f
Leo Zubiri <Leo.Zubiri@outlook.com>
2025-10-15 17:32:41 -0600

    fix. first map loading center on CDMX

--- assets/map.js
+++ assets/map.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 19.432608, lng: -99.133209 },
     zoom: 8,
   });
 }
